f64001d77ccfda89e98c7fc6ae9fe3486e4b89df
Crosson David <crosson.david@gmail.com>
2019-11-22 18:26:01 +0100

    Add more tests

--- src/main/scala/fr/janalyse/elasticsearch/client/scala/ElasticClientHelper.scala
+++ src/main/scala/fr/janalyse/elasticsearch/client/scala/ElasticClientHelper.scala
@@ -2,8 +2,13 @@ package fr.janalyse.elasticsearch.client.scala
 
 import com.sksamuel.elastic4s.{ElasticClient, ElasticProperties}
 import com.sksamuel.elastic4s.http.JavaClient
+import org.json4s.ext.JavaTimeSerializers
+import org.json4s.{DefaultFormats, native}
 
 trait ElasticClientHelper {
+  implicit val serialization = native.Serialization
+  implicit val formats = DefaultFormats.lossless ++ JavaTimeSerializers.all
+
   def getElasticClient(elasticPort:Int = 9201) = {
     ElasticClient(JavaClient(ElasticProperties(s"http://127.0.0.1:$elasticPort")))
   }
--- src/test/scala/fr/janalyse/elasticsearch/client/scala/BasicsTest.scala
+++ src/test/scala/fr/janalyse/elasticsearch/client/scala/BasicsTest.scala
@@ -1,15 +1,110 @@
 package fr.janalyse.elasticsearch.client.scala
 
 import com.sksamuel.elastic4s.ElasticDsl._
+import com.sksamuel.elastic4s.json4s.ElasticJson4s.Implicits._
+
 
 class BasicsTest extends ElasticClientTestsHelper {
   // ----------------------------------------------------------------------
   "elasticsearch client application" should "be able to get cluster state information" in {
     client.execute {
       clusterState()
-    } map { response =>
+    }.map { response =>
       response.result.nodes.size should be > 0
+      response.result.clusterName.size should be > 0
     }
   }
 
+  it should "be able to create an index" in {
+    client.execute {
+      createIndex("basics").shards(3).replicas(1)
+    }.map { response =>
+      response.isSuccess shouldBe true
+    }
+  }
+
+  it should "be able to append a new document" in {
+    client.execute {
+      indexInto("basics")
+        .doc(Map("msg"->"Hello World"))
+    }.map {response =>
+      response.isSuccess shouldBe true
+    }
+  }
+
+  it should "be able to append a new document with a specified id" in {
+    client.execute {
+      indexInto("basics")
+        .id("42")
+        .doc(Map("msg"->"Hello World"))
+    }.map {response =>
+      response.isSuccess shouldBe true
+    }
+  }
+
+  it should "be able to update an existing document" in {
+    client.execute {
+      update("42")
+        .in("basics")
+        .doc(Map("msg"->"Hello World !!"))
+    }.map {response =>
+      response.isSuccess shouldBe true
+    }
+  }
+
+  it should "be able to get a document" in {
+    client.execute {
+      get("42").from("basics")
+    }.map {response =>
+      response.result.source.size should be > 0
+      response.result.version shouldBe 2
+    }
+  }
+
+  it should "be able to update/INSERT a document" in {
+    info("If the document doesn't exist it is inserted, if it exists then it is updated")
+    client.execute {
+      update("4242")
+        .in("basics")
+        .docAsUpsert(Map("msg"-> "Bonjour tout le monde !"))
+    }.map {response =>
+      response.isSuccess shouldBe true
+    }
+  }
+
+  it should "be able to UPDATE/insert a document" in {
+    info("If the document doesn't exist it is inserted, if it exists then it is updated")
+    client.execute {
+      update("4242")
+        .in("basics")
+        .docAsUpsert(Map("msg"-> "Bonjour tout le monde !!"))
+    }.map {response =>
+      response.isSuccess shouldBe true
+    }
+  }
+
+  it should "be able to refresh explicitly an index" in {
+    info("Take care of performance, this is not something you should do")
+    client.execute {
+      refreshIndex("basics")
+    }.map{ response =>
+      response.isSuccess shouldBe true
+    }
+  }
+
+  it should "be able to count documents" in {
+    client.execute {
+      count("basics")
+    }.map {response =>
+      response.result.count shouldBe 3L
+    }
+  }
+
+  it should "be able to delete an index" in {
+    client.execute {
+      deleteIndex("basics")
+    }.map { response =>
+      response.isSuccess shouldBe true
+    }
+  }
 }
